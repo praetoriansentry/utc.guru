5e670f8412a864ae0dfa759dd875c39674a2e7f1
John Hilliard <praetoriansentry@gmail.com>
2018-07-23 16:49:48 +0000

    init commit

---
+++ src/main.go
@@ -0,0 +1,58 @@
+package main
+
+import (
+	"context"
+	"github.com/aws/aws-lambda-go/lambda"
+	"time"
+)
+
+type Times struct {
+	UTC         time.Time
+	ANSIC       string
+	UnixDate    string
+	RubyDate    string
+	RFC822      string
+	RFC822Z     string
+	RFC850      string
+	RFC1123     string
+	RFC1123Z    string
+	RFC3339     string
+	RFC3339Nano string
+	Kitchen     string
+	Stamp      string
+	StampMilli string
+	StampMicro string
+	StampNano  string
+	Unix       int64
+	UnixNano   int64
+}
+
+func HandleRequest(ctx context.Context) (Times, error) {
+	t := time.Now().UTC()
+	ts := Times{
+		UTC: t,
+		ANSIC: t.Format(time.ANSIC),
+		UnixDate: t.Format(time.UnixDate),
+		RubyDate: t.Format(time.RubyDate),
+		RFC822: t.Format(time.RFC822),
+		RFC822Z: t.Format(time.RFC822Z),
+		RFC850: t.Format(time.RFC850),
+		RFC1123: t.Format(time.RFC1123),
+		RFC1123Z: t.Format(time.RFC1123Z),
+		RFC3339: t.Format(time.RFC3339),
+		RFC3339Nano: t.Format(time.RFC3339Nano),
+		Kitchen: t.Format(time.Kitchen),
+		Stamp: t.Format(time.Stamp),
+		StampMilli: t.Format(time.StampMilli),
+		StampMicro: t.Format(time.StampMicro),
+		StampNano: t.Format(time.StampNano),
+		Unix: t.Unix(),
+		UnixNano: t.UnixNano(),
+
+	}
+	return ts, nil
+}
+
+func main() {
+	lambda.Start(HandleRequest)
+}
